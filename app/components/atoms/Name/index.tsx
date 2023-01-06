c9018450c9d7bc8d7f020682ac12a952696b381b
Giorgio Torre <giorgio.torre8@gmail.com>
2023-01-06 11:39:54 +0100

    removed unused files, modified footer background

--- app/components/atoms/Name/index.tsx
+++ app/components/atoms/Name/index.tsx
@@ -1,9 +1,14 @@
+import { NavLink } from "@remix-run/react";
 import * as S from "./styles";
 
 type NameProps = {
   text: string;
 };
 
-const Name = ({ text }: NameProps) => <S.Text>{text}</S.Text>;
+const Name = ({ text }: NameProps) => (
+  <NavLink to="/">
+    <S.Text>{text}</S.Text>
+  </NavLink>
+);
 
 export default Name;
--- app/components/molecules/Footer/styles.ts
+++ app/components/molecules/Footer/styles.ts
@@ -1,7 +1,9 @@
 import styled from "styled-components";
+import { colors } from "~/common/constants";
 
 export const Footer = styled.footer`
   display: flex;
   justify-content: center;
-  padding: 20px;
+  padding: 40px 0px;
+  background-color: ${colors.offGray};
 `;
