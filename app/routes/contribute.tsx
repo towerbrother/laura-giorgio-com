7e5e2e5a9cdf5a595e915f4b4157094bb32597c0
GTO <giorgio.torre@stellenanzeigen.de>
2023-01-10 17:29:41 +0100

    reminder in contribute page

--- app/routes/contribute.tsx
+++ app/routes/contribute.tsx
@@ -5,6 +5,17 @@ export default function Index() {
   return (
     <>
       <BankDetails {...bankDetails} />
+      <div
+        style={{
+          backgroundColor: 'red',
+          color: 'white',
+          textAlign: 'center',
+          fontSize: '32px',
+          padding: '30px 0px',
+        }}
+      >
+        PHOTOS
+      </div>
     </>
   );
 }
