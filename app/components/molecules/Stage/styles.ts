a3e37735e666ece157f1f91ef5d1413497f59254
GTO <giorgio.torre@stellenanzeigen.de>
2023-01-13 20:28:00 +0100

    restyled stage

--- app/components/molecules/Stage/styles.ts
+++ app/components/molecules/Stage/styles.ts
@@ -6,41 +6,48 @@ export const Stage = styled.div`
   position: relative;
 `;
 
-export const Image = styled.img``;
+export const Image = styled.img`
+  width: 100%;
+`;
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: center;
   text-align: center;
   width: 100%;
-  height: 90%;
-  padding: 0px 10px;
+  height: 100%;
   position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
+  top: 30px;
   color: ${colors.offWhite};
   font-weight: 700;
 
-  @media ${devices.laptop} {
-    height: 70%;
+  @media ${devices.tablet} {
+    top: 20px;
   }
 `;
 
 export const Title = styled.h2`
-  font-size: 36px;
   font-style: italic;
   word-spacing: -5px;
+  font-size: 28px;
+
+  @media ${devices.tablet} {
+    font-size: 44px;
+  }
 
   @media ${devices.laptop} {
-    font-size: 68px;
+    font-size: 62px;
   }
 `;
 
 export const SubTitle = styled.h4`
-  font-size: 12px;
   text-transform: uppercase;
+  font-size: 12px;
+
+  @media ${devices.tablet} {
+    font-size: 16px;
+  }
 
   @media ${devices.laptop} {
     font-size: 20px;
@@ -48,10 +55,16 @@ export const SubTitle = styled.h4`
 `;
 
 export const Date = styled.span`
-  font-size: 24px;
+  margin: 10px 0px;
+  font-size: 16px;
+
+  @media ${devices.tablet} {
+    margin: 20px 0px;
+    font-size: 28px;
+  }
 
   @media ${devices.laptop} {
-    font-size: 32px;
+    font-size: 36px;
   }
 `;
 
@@ -63,11 +76,16 @@ export const Link = styled(NavLink)`
   width: max-content;
   margin: 0 auto;
   font-size: 16px;
-  padding: 10px 24px;
+  padding: 6px 12px;
 
   &:hover {
     transition: all 0.2s ease-in-out;
     background: ${colors.offGray};
+  }
+
+  @media ${devices.tablet} {
+    font-size: 24px;
+    padding: 10px 22px;
   }
 
   @media ${devices.laptop} {
--- app/globalStyles.ts
+++ app/globalStyles.ts
@@ -44,7 +44,6 @@ export const GlobalStyle = createGlobalStyle`
   video,
   canvas,
   svg {
-    display: block;
     max-width: 100%;
   }
 
