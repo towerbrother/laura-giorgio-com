8a862e2e011ebff3e5c2eb2c37b962d8231c6a0e
GTO <giorgio.torre@stellenanzeigen.de>
2023-01-22 22:21:33 +0100

    Login styles amended

--- app/components/molecules/Login.tsx
+++ app/components/molecules/Login.tsx
@@ -2,7 +2,7 @@ import { Form } from '@remix-run/react';
 import Button from '~/components/atoms/Button';
 
 const Login = () => (
-  <div className="w-screen h-screen flex flex-col justify-center items-center bg-gradient-to-br from-cyan-100 to-blue-800">
+  <div className="w-screen h-screen flex flex-col justify-center items-center bg-gradient-to-br from-blue-800 to-cyan-100">
     <Form
       method="post"
       action="/set-user-auth"
@@ -12,12 +12,12 @@ const Login = () => (
         name="password"
         type="password"
         placeholder="Password..."
-        className="text-2xl bg-neutral-100 border-solid border-emerald-600 shadow-lg border-2 rounded-md py-2 px-3 mb-3 w-72 md:w-96 placeholder:text-neutral-400 placeholder:text-2xl focus:outline-none"
+        className="text-2xl bg-neutral-100 border-none shadow-lg border-2 rounded-md py-2 px-3 mb-3 w-72 sm:w-80 md:w-96 placeholder:text-neutral-800 placeholder:text-2xl focus:outline-none"
         required
       />
       <Button
         type="submit"
-        className="text-2xl py-2 font-bold text-neutral-100 bg-emerald-600 shadow-lg rounded-md hover:opacity-80 w-72 md:w-96"
+        className="text-2xl py-2 font-bold text-neutral-100 bg-neutral-800 shadow-lg rounded-md hover:opacity-80 w-72 sm:w-80 md:w-96"
       >
         LOGIN
       </Button>
